d7b2dc1c022b420a027b2284e8d777959796566b
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2021-10-14 23:10:13 -0300

    Modulo Clusterting

---
+++ 4._Clustering/kmeans.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 22:43:49 2021
+
+@author: plerzundi
+@description: KMEANS MODEL
+"""
+
+# Librerias principales
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Importar Dataset
+df = pd.read_csv("Mall_Customers.csv")
+
+
